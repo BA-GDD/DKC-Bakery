a375c44071df00e4cc7118fdacd6e3ab2c1261be
botw0303 <zeldabotw0303@gmail.com>
2023-12-20 18:33:51 +0900

    ready to merge

--- Assets/01.Scripts/Items/ItemDataSO.cs
+++ Assets/01.Scripts/Items/ItemDataSO.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 public enum ItemType
 {
     Material,
-    Equipment
+    Equipment,
+    Paste,
+    Ingredient
 }
 
 [CreateAssetMenu(menuName = "SO/Items/Item")]
@@ -13,6 +15,7 @@ public class ItemDataSO : ScriptableObject
     public ItemType itemType;
     public string itemName;
     public Sprite itemIcon;
+    public string description;
 
     protected StringBuilder _stringBuilder = new StringBuilder();
 
