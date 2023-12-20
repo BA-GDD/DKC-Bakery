abeac5921e5ef8295566aed0e4092dafae4092d0
VsPot07 <cocoacake84@gmail.com>
2023-12-20 19:01:04 +0900

    1220 작동하면 그만이야 한잔 해

--- Assets/01.Scripts/Hogamdo/SOscripts/LikebilityData.cs
+++ Assets/01.Scripts/Hogamdo/SOscripts/LikebilityData.cs
@@ -1,14 +1,32 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEditor;
 
 [CreateAssetMenu(menuName = "SO/Hogam/LikeabilityShameTable")]
 public class LikebilityData : LoadableData
 {
     public int NeedShameToLevelUp(int level)
     {
-        Debug.Log(generateData[level].str[1]);
         return Convert.ToInt32(generateData[level].str[1]);
     }
 }
+
+[CustomEditor(typeof(LikebilityData))]
+public class LikebilityDataLoader : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LikebilityData likebilityData = (LikebilityData)target;
+        LoadableData ld = likebilityData;
+        
+        if (GUILayout.Button("LikebilityDataReading"))
+        {
+            ld.Generate();
+        }
+    }
+}
+
 
