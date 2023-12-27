3b78100ac851ed3b2902c2441d6c901136cd7a71
Gusdnd01 <cestop42@naver.com>
2023-12-28 00:41:46 +0900

    검 시시싱, 펑!

--- Assets/01.Scripts/FX/ExplosionScreen.cs
+++ Assets/01.Scripts/FX/ExplosionScreen.cs
@@ -11,6 +11,7 @@ public class ExplosionScreen : MonoBehaviour
     [SerializeField] private GameObject _renderCam;
 
     [SerializeField] private GameObject _shatterEffectObj;
+    [SerializeField] private Transform _explosionTrm;
     [SerializeField] private ParticleSystem _swordScreenEffect;
 
     [SerializeField] private float _explosionPow;
@@ -35,14 +36,14 @@ public class ExplosionScreen : MonoBehaviour
         _swordScreenEffect.Play();
         yield return new WaitForSeconds(1.6f);
 
-        var obj = Instantiate(_shatterEffectObj, Camera.main.transform.position, Quaternion.identity);
-        obj.transform.position += new Vector3(0,0,10);
+        var obj = Instantiate(_shatterEffectObj, _renderCam.transform.position, Quaternion.identity);
+        obj.transform.position += new Vector3(0,0,5);
         obj.transform.rotation = Quaternion.Euler(0, 0, 180);
         _explosionRbs = obj.GetComponentsInChildren<Rigidbody>();
 
         foreach(var rb in _explosionRbs)
         {
-            rb.AddExplosionForce(_explosionPow, _renderCam.transform.position, _explosionRad);
+            rb.AddExplosionForce(_explosionPow, _explosionTrm.position, _explosionRad);
         }
 
         yield return new WaitForSeconds(3.0f);
