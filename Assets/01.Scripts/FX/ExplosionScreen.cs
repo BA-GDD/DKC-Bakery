7931f5d3ff9eda0d8efb2d67d9f908cc5261ad16
Gusdnd01 <cestop42@naver.com>
2023-12-28 00:25:51 +0900

    검 시시싱, 챙!

---
+++ Assets/01.Scripts/FX/ExplosionScreen.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class ExplosionScreen : MonoBehaviour
+{
+
+    //디버그용 스크립트
+    private Rigidbody[] _explosionRbs;
+
+    [SerializeField] private GameObject _renderCam;
+
+    [SerializeField] private GameObject _shatterEffectObj;
+    [SerializeField] private ParticleSystem _swordScreenEffect;
+
+    [SerializeField] private float _explosionPow;
+    [SerializeField] private float _explosionRad;
+
+    private void Update()
+    {
+        if (Keyboard.current.fKey.wasPressedThisFrame)
+        {
+            
+            StartCoroutine(Shatter());
+        }
+    }
+
+    private IEnumerator Shatter()
+    {
+
+        _renderCam.SetActive(true);
+        yield return new WaitForEndOfFrame();
+        _renderCam.SetActive(false);
+
+        _swordScreenEffect.Play();
+        yield return new WaitForSeconds(1.6f);
+
+        var obj = Instantiate(_shatterEffectObj, Camera.main.transform.position, Quaternion.identity);
+        obj.transform.position += new Vector3(0,0,10);
+        obj.transform.rotation = Quaternion.Euler(0, 0, 180);
+        _explosionRbs = obj.GetComponentsInChildren<Rigidbody>();
+
+        foreach(var rb in _explosionRbs)
+        {
+            rb.AddExplosionForce(_explosionPow, _renderCam.transform.position, _explosionRad);
+        }
+
+        yield return new WaitForSeconds(3.0f);
+
+        Destroy(obj);
+    }
+}
