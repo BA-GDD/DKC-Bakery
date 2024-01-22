740f7d326abd0301a09b7ddd01889a429d673919
VsPot07 <cocoacake84@gmail.com>
2024-01-23 04:13:21 +0900

    0123

---
+++ Assets/01.Scripts/UI/Option/InfoBlockSelectBtn.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class InfoBlockSelectBtn : MonoBehaviour
+{
+    [Header("셋팅값")]
+    [SerializeField] private TextMeshProUGUI _selectText;
+    [SerializeField] private Image _btnImg;
+    [SerializeField] private CanvasGroup _markBlock;
+    [SerializeField] private bool _clickThisPanel;
+    public bool ClickThisPanel
+    {
+        get
+        {
+            return _clickThisPanel;
+        }
+        set
+        {
+            _clickThisPanel = value;
+            HandlePointerClickBtn(_clickThisPanel);
+            HandleOutPointerBtn();
+        }
+    }
+
+    [Header("수치")]
+    [SerializeField] private float _selectMoveValue;
+    [SerializeField] private float _colorBlurValue;
+    [SerializeField] private float _easingTime;
+
+    private Color _blurColor;
+    private Vector2 _normalPos;
+    private Vector2 _selectPos;
+
+    private void Start()
+    {
+        _normalPos = transform.localPosition;
+        _selectPos = _normalPos + new Vector2(_selectMoveValue, 0);
+
+        _blurColor = Color.white * _colorBlurValue;
+        _blurColor.a = 1;
+
+        if(_clickThisPanel)
+        {
+            HandlePointerClickBtn(_clickThisPanel);
+        }
+        else
+        {
+            HandleOutPointerBtn();
+        }
+    }
+
+    public void HandleOnPointerBtn()
+    {
+        if (_selectText.color == Color.white) return;
+
+        _selectText.DOColor(Color.white, _easingTime);
+    }
+
+    public void HandleOutPointerBtn()
+    {
+        if (_clickThisPanel) return;
+
+        _selectText.DOColor(_blurColor, _easingTime);
+    }
+
+    private void HandlePointerClickBtn(bool isSelect)
+    {
+        _markBlock.enabled = isSelect;
+        _markBlock.interactable = isSelect;
+
+        Vector2 movingPos = isSelect ? _selectPos : _normalPos;
+        Color btnColor = isSelect ? Color.white : _blurColor;
+
+        _btnImg.transform.DOLocalMove(movingPos, _easingTime);
+        _btnImg.DOColor(btnColor, _easingTime);
+    }
+}
