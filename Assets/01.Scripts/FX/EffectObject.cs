a931cff598ceea3c14c70a364d0ac87a9b4afcb9
Gusdnd01 <cestop42@naver.com>
2024-03-06 00:39:32 +0900

    Effect 추가

--- Assets/01.Scripts/FX/EffectObject.cs
+++ Assets/01.Scripts/FX/EffectObject.cs
@@ -2,17 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EffectObject : MonoBehaviour
+public class EffectObject : PoolableMono
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private ParticleSystem mainParticleSystem;
+
+    [SerializeField]
+    private float pushTime = 1.0f;
+
+    public override void Init()
     {
-        
+        mainParticleSystem.Stop();
+        mainParticleSystem.Play();
+
+        StartCoroutine(WaitForEnd());
     }
 
-    // Update is called once per frame
-    void Update()
+    private void PushThis()
     {
-        
+        mainParticleSystem.Stop();
+        PoolManager.Instance.Push(this);
+    }
+
+    private IEnumerator WaitForEnd()
+    {
+        yield return new WaitForSeconds(pushTime);
+        PushThis();
     }
 }
