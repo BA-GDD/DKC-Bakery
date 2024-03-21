854ad293ca9d6c79fc273274b10ad43a6f5388f1
botw0303 <101687115+botw0303@users.noreply.github.com>
2024-03-21 12:41:30 +0900

    commit for merge

---
+++ Assets/01.Scripts/Card/Skill/JaggieLanternSkill.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JaggieLanternSkill : CardBase, ISkillEffectAnim
+{
+    public override void Abillity()
+    {
+        IsActivingAbillity = true;
+        Player.UseAbility(this);
+        Player.OnAnimationCall += HandleAnimationCall;
+        Player.VFXManager.OnEndEffectEvent += HandleEffectEnd;
+    }
+
+    public void HandleAnimationCall()
+    {
+        Player.VFXManager.PlayParticle(CardInfo);
+        StartCoroutine(AttackCor());
+        Player.OnAnimationCall -= HandleAnimationCall;
+    }
+
+    public void HandleEffectEnd()
+    {
+        Player.EndAbility();
+        Player.VFXManager.EndParticle(CardInfo);
+        IsActivingAbillity = false;
+        Player.VFXManager.OnEndEffectEvent -= HandleEffectEnd;
+    }
+
+    private IEnumerator AttackCor()
+    {
+
+    }
+}
