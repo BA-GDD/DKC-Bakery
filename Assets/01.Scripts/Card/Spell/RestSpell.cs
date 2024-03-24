288d34ec92dc6658043f4ff8534129c1c2abf913
botw0303 <101687115+botw0303@users.noreply.github.com>
2024-03-24 21:34:05 +0900

    commit for merge

---
+++ Assets/01.Scripts/Card/Spell/RestSpell.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestSpell : CardBase, IUseEffectAnim
+{
+    public override void Abillity()
+    {
+        IsActivingAbillity = true;
+        Player.UseAbility(this);
+        Player.OnAnimationCall += HandleAnimationCall;
+        Player.VFXManager.OnEndEffectEvent += HandleEffectEnd;
+    }
+
+    public void HandleAnimationCall()
+    {
+        Player.VFXManager.PlayParticle(CardInfo);
+        StartCoroutine(SpellCor());
+        Player.OnAnimationCall -= HandleAnimationCall;
+    }
+
+    public void HandleEffectEnd()
+    {
+        Player.EndAbility();
+        Player.VFXManager.EndParticle(CardInfo);
+        IsActivingAbillity = false;
+        Player.VFXManager.OnEndEffectEvent -= HandleEffectEnd;
+    }
+
+    private IEnumerator SpellCor()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+}
