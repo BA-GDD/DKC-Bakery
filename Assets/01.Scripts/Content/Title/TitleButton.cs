74d3f3bdf16ff8479508392bf797fec4f8959c70
janghawon <98999600+janghawon@users.noreply.github.com>
2024-03-24 18:25:40 +0900

    0324

--- Assets/01.Scripts/Content/Title/TitleButton.cs
+++ Assets/01.Scripts/Content/Title/TitleButton.cs
@@ -6,35 +6,25 @@ using UnityEngine.UI;
 
 public abstract class TitleButton : MonoBehaviour
 {
-    [SerializeField] private RectTransform _labelRect;
     [SerializeField] private Image _labelImg;
 
-    [SerializeField] private float _startXValue;
-    [SerializeField] private float _endXValue;
-    [SerializeField] private Color _normalColor;
+    [SerializeField] private float _startValue;
+    [SerializeField] private float _endValue;
 
     private Tween _appearTween;
-    private Sequence _disappearSeq;
+    private Tween _disappearTween;
 
     public void HobberEvent()
     {
-        _disappearSeq.Kill();
+        _disappearTween.Kill();
 
-        _labelImg.enabled = true;
-        _labelRect.localPosition = new Vector3(_startXValue, _labelRect.transform.localPosition.y);
-        _labelRect.localScale = Vector3.one;
-        _labelImg.color = _normalColor;
-
-        _appearTween = _labelRect.DOLocalMoveX(_endXValue, 0.2f);
+        _appearTween = DOTween.To(() => _startValue, sd => _labelImg.fillAmount = sd, _endValue, 0.1f);
     }
     public void UnHobberEvent()
     {
         _appearTween.Kill();
 
-        _disappearSeq = DOTween.Sequence();
-        _disappearSeq.Append(_labelRect.DOScaleY(0, 0.4f));
-        _disappearSeq.Join(_labelImg.DOFade(0, 0.4f));
-        _disappearSeq.AppendCallback(()=> _labelImg.enabled = false);
+        _disappearTween = DOTween.To(() => _labelImg.fillAmount, sd => _labelImg.fillAmount = sd, _startValue, 0.1f);
     }
     public abstract void PressEvent();
 }
