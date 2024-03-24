492778787da2fd9dc972bbd450f4ddd002d4271f
hueam <72968569+hueam@users.noreply.github.com>
2024-03-24 21:33:19 +0900

    asdf

--- Assets/01.Scripts/Entity.cs
+++ Assets/01.Scripts/Entity.cs
@@ -2,6 +2,7 @@ using Cinemachine;
 using DG.Tweening;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEditorInternal;
 using UnityEngine;
@@ -28,7 +29,7 @@ public abstract class Entity : PoolableMono
     public Action OnMoveTarget;
     public Action OnMoveOriginPos;
 
-    public Action OnAttack;
+    public List<IOnTakeDamage> OnAttack;
 
     public Transform forwardTrm;
 
@@ -37,6 +38,8 @@ public abstract class Entity : PoolableMono
     [SerializeField] protected Vector3 lastMovePos;
     [SerializeField] protected float moveDuration = 0.1f;
 
+    private SkillCardManagement management;
+    public UnityEvent BeforeChainingEvent => management.beforeChainingEvent;
 
     protected virtual void Awake()
     {
@@ -55,6 +58,8 @@ public abstract class Entity : PoolableMono
         OnHealthBarChanged?.Invoke(HealthCompo.GetNormalizedHealth()); //�ִ�ġ�� UI����.
         CharStat = Instantiate(CharStat); //������ ����
         CharStat.SetOwner(this);
+
+        management = FindObjectOfType<SkillCardManagement>();
 
         OnMoveTarget += HandleMoveToTarget;
         OnMoveOriginPos += HandleMoveToOriginPos;
@@ -111,16 +116,14 @@ public abstract class Entity : PoolableMono
         AnimatorCompo.speed = 1; //���� ���ǵ�� �ǵ�����.
     }
 
-    public virtual void FreezeTime(bool isFreeze, bool isFrozenWithoutTimer = false)
+    public virtual void Freeze(bool isFreeze)
     {
         if (isFreeze)
         {
-            Debug.Log("Freezed");
             AnimatorCompo.speed = 0; //�ִϸ��̼� ����. �̵� ����.
         }
         else
         {
-            Debug.Log("UnFreezed");
             AnimatorCompo.speed = 1;
         }
     }
@@ -154,7 +157,7 @@ public abstract class Entity : PoolableMono
             SpriteRendererCompo.material.SetFloat("_dissolve_amount",Mathf.Lerp(0,1,timer));
             yield return null;
         }
-
+        PoolManager.Instance.Push(this);
     }
 
     public override void Init()
