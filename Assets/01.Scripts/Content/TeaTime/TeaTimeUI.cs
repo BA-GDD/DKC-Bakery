513f7fdc91d7c1ca82c3d4c8d5ef0dacd4f0d162
janghawon <98999600+janghawon@users.noreply.github.com>
2024-03-28 19:18:39 +0900

    0328

--- Assets/01.Scripts/Content/TeaTime/TeaTimeUI.cs
+++ Assets/01.Scripts/Content/TeaTime/TeaTimeUI.cs
@@ -4,4 +4,10 @@ using UnityEngine;
 
 public class TeaTimeUI : SceneUI
 {
+    [SerializeField] private EatRange _eatRange;
+    public EatRange EatRange => _eatRange;
+    [SerializeField] private TeaTimeCreamStand _creamStand;
+    public TeaTimeCreamStand TeaTimeCreamStand => _creamStand;
+    [SerializeField] private CakeCollocation _cakeCollocation;
+    public CakeCollocation CakeCollocation => _cakeCollocation;
 }
