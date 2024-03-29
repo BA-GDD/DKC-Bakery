7ccb24de843b4b9034001fde06e2d34d47844459
janghawon <98999600+janghawon@users.noreply.github.com>
2024-03-29 23:37:03 +0900

    0329

--- Assets/01.Scripts/Core/Manager/DeckManager.cs
+++ Assets/01.Scripts/Core/Manager/DeckManager.cs
@@ -20,6 +20,18 @@ public class DeckManager : MonoSingleton<DeckManager>
         return _getCardDic[cardName];
     }
 
+    public List<string> GetDeckData(List<CardBase> deck)
+    {
+        List<string> data = new List<string>();
+
+        foreach(CardBase card in deck)
+        {
+            data.Add(card.CardInfo.CardName);
+        }
+
+        return data;
+    }
+
     public List<CardBase> GetDeck(List<string> deckData)
     {
         List<CardBase> _deck = new ();
