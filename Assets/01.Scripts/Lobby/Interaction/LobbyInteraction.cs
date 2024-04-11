3fe95b666cbd31ce62cc9c9e0bee01bb835feeb7
janghawon <98999600+janghawon@users.noreply.github.com>
2024-04-11 10:23:53 +0900

    0411

---
+++ Assets/01.Scripts/Lobby/Interaction/LobbyInteraction.cs
@@ -0,0 +1,36 @@
+using EpisodeDialogueDefine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyInteraction : MonoBehaviour
+{
+    private Dictionary<CharacterType, CharacterStand> _characterDic = new ();
+    [SerializeField] private List<EpisodeData> _interactionData = new ();
+
+    private void Start()
+    {
+        CharacterStand[] cs = GetComponentsInChildren<CharacterStand>();
+        foreach (CharacterStand c in cs)
+        {
+            _characterDic.Add(c.CharacterType, c);
+            c.InterAction = this;
+        }
+    }
+
+    public void StartInteraction()
+    {
+        int rdIdx = Random.Range(0, _interactionData.Count);
+        EpisodeData episodeData = _interactionData[rdIdx];
+
+        //StartCoroutine(InteractionSequence(episodeData));
+    }
+
+    //IEnumerator InteractionSequence(EpisodeData episodeData)
+    //{
+    //    for(int i = 0; i < episodeData.dialogueElement.Count; i++)
+    //    {
+    //        Character
+    //    }
+    //}
+}
