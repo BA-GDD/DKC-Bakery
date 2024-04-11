aaa5b44d514d959cda2d0a01ef1180c81dd20f66
janghawon <98999600+janghawon@users.noreply.github.com>
2024-04-12 00:25:57 +0900

    04122 dutdl

--- Assets/01.Scripts/Card/CostCheck.cs
+++ Assets/01.Scripts/Card/CostCheck.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,7 +8,10 @@ using UnityEngine.UI;
 
 public class CostCheck : MonoBehaviour
 {
+    private int _currentMana = 10;
     [SerializeField] private TextMeshProUGUI _costText;
+
+    [SerializeField] private Image[] _manaArr;
     [SerializeField] private Image[] _extramanaArr;
 
     private void Start()
@@ -36,6 +40,30 @@ public class CostCheck : MonoBehaviour
 
     private void HandleCheckCost(int currentMoney)
     {
+        int diffMana =  currentMoney - _currentMana;
+
+        Debug.Log(diffMana);
+        if(diffMana > 0 )
+        {
+            for(int i = _currentMana; i < currentMoney + 1; i++)
+            {
+                Material mat = new Material(_manaArr[i].material);
+                _manaArr[i].material = mat;
+                DOTween.To(() => -1, v => mat.SetFloat("_dissolve_amount", v), 1, 0.4f);
+            }
+        }
+        else
+        {
+            Debug.Log($"{_currentMana}, {currentMoney}");
+            for(int i = _currentMana; i > currentMoney; i--)
+            {
+                Material mat = new Material(_manaArr[i].material);
+                _manaArr[i].material = mat;
+                DOTween.To(() => 1, v => mat.SetFloat("_dissolve_amount", v), -1, 0.4f);
+            }
+        }
+
+        _currentMana = currentMoney;
         _costText.text = currentMoney.ToString();
     }
 
@@ -43,12 +71,12 @@ public class CostCheck : MonoBehaviour
     {
         for (int i = 0; i < _extramanaArr.Length; i++)
         {
-            _extramanaArr[i].enabled = false;
+            //_extramanaArr[i].enabled = false;
         }
 
         for (int i = 0; i < currentMana; i++)
         {
-            _extramanaArr[i].enabled = true;
+            //_extramanaArr[i].enabled = true;
         }
     }
 }
