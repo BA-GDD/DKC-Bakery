3d702a42e739991dc24b2f43319ef6e9a4a2cbee
botw0303 <101687115+botw0303@users.noreply.github.com>
2024-05-28 00:48:10 +0900

    finding bug

--- Assets/01.Scripts/Card/Skill/GrindingSkill.cs
+++ Assets/01.Scripts/Card/Skill/GrindingSkill.cs
@@ -34,7 +34,7 @@ public class GrindingSkill : CardBase, ISkillEffectAnim
     {
         yield return new WaitForSeconds(0.3f);
 
-        Player.BuffStatCompo.AddStack(StackEnum.Forging, (int)CombineLevel);
+        Player.BuffStatCompo.AddStack(StackEnum.Forging, (int)CombineLevel + 1);
         Player.BuffStatCompo.AddBuff(buffSO, 2, (int)CombineLevel);
 
         Debug.Log($"Current Forging Stat: {Player.BuffStatCompo.GetStack(StackEnum.Forging)}");
