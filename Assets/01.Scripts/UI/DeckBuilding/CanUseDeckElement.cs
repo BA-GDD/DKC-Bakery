6f2640f10458dd570c4e189af029bb0b9176ea14
botw0303 <101687115+botw0303@users.noreply.github.com>
2024-05-27 18:55:38 +0900

    Update CanUseDeckElement.cs

--- Assets/01.Scripts/UI/DeckBuilding/CanUseDeckElement.cs
+++ Assets/01.Scripts/UI/DeckBuilding/CanUseDeckElement.cs
@@ -1,14 +1,20 @@
+using DG.Tweening;
+using ExtensionFunction;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class CanUseDeckElement : MonoBehaviour, IPointerClickHandler
+public class CanUseDeckElement : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public DeckElement DeckInfo { get; private set; }
 
+    [SerializeField] private Image[] _cardGroupArr = new Image[5];
     [SerializeField] private TextMeshProUGUI _deckNameText;
+    [SerializeField] private float _onPointerEnterMoveY;
+    [SerializeField] private float _onPointerExitMoveY;
     private DeckElement _deckInfo;
 
     private DeckGenerator _deckGenerator;
@@ -24,19 +30,19 @@ public class CanUseDeckElement : MonoBehaviour, IPointerClickHandler
         DeckInfo = deckInfo;
         _deckGenerator = deckGenerator;
 
-        //for(int i = 0; i < _cardGroupArr.Length; i++)
-        //{
-        //    CardBase card = DeckManager.Instance.GetCard(deckInfo.deck[i]);
-        //    _cardGroupArr[i].sprite = card.CardInfo.CardVisual;
-        //    TextMeshProUGUI costText = _cardGroupArr[i].GetComponentInChildren<TextMeshProUGUI>();
-        //    costText.text = card.AbilityCost.ToString();
-        //}
+        for (int i = 0; i < _cardGroupArr.Length; i++)
+        {
+            CardBase card = DeckManager.Instance.GetCard(deckInfo.deck[i]);
+            _cardGroupArr[i].sprite = card.CardInfo.CardVisual;
+            TextMeshProUGUI costText = _cardGroupArr[i].GetComponentInChildren<TextMeshProUGUI>();
+            costText.text = card.AbilityCost.ToString();
+        }
 
         _deckName = deckInfo.deckName;
 
-        if(_deckName.Length > 6)
+        if (_deckName.Length > 6)
         {
-            _deckName = $"{_deckName.Substring(0, 6)}.."; 
+            _deckName = $"{_deckName.Substring(0, 6)}..";
         }
 
         _deckNameText.text = _deckName;
@@ -45,36 +51,35 @@ public class CanUseDeckElement : MonoBehaviour, IPointerClickHandler
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        //transform.DOKill();
-        //transform.DOScale(1.05f, 0.3f);
+        transform.DOKill();
+        transform.DOScale(1.05f, 0.3f);
 
-        //for (int i = -2; i <= 2; i++)
-        //{
-        //    Transform trm = _cardGroupArr[i + 2].transform;
+        for (int i = -2; i <= 2; i++)
+        {
+            Transform trm = _cardGroupArr[i + 2].transform;
 
-        //    trm.DOKill();
-        //    trm.DOLocalMove(new Vector2(_cardGroupArr[i + 2].transform.localPosition.x + (i * 5),
-        //                                _onPointerEnterMoveY + (2 - Mathf.Abs(i)) * 5), 0.3f).SetEase(Ease.OutBack);
-        //    trm.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -i * 5), 0.3f).SetEase(Ease.OutBack);
-        //}
+            trm.DOKill();
+            trm.DOLocalMove(new Vector2(_cardGroupArr[i + 2].transform.localPosition.x + (i * 5),
+                                        _onPointerEnterMoveY + (2 - Mathf.Abs(i)) * 5), 0.3f).SetEase(Ease.OutBack);
+            trm.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, -i * 5), 0.3f).SetEase(Ease.OutBack);
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        //    transform.DOKill();
-        //    transform.DOScale(1f, 0.3f);
+        transform.DOKill();
+        transform.DOScale(1f, 0.3f);
 
-        //    for (int i = 0; i < _cardGroupArr.Length; i++)
-        //    {
-        //        float value = (i - 2) * 5;
+        for (int i = 0; i < _cardGroupArr.Length; i++)
+        {
+            float value = (i - 2) * 5;
 
-        //        Transform trm = _cardGroupArr[i].transform;
+            Transform trm = _cardGroupArr[i].transform;
 
-        //        trm.DOKill();
-        //        trm.DOLocalMove(new Vector2(_cardGroupArr[i].transform.localPosition.x - value,
-        //                                    _onPointerExitMoveY), 0.3f).SetEase(Ease.OutBack);
-        //        trm.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, 0), 0.3f).SetEase(Ease.OutBack);
-        //    }
-        //}
+            trm.DOKill();
+            trm.DOLocalMove(new Vector2(_cardGroupArr[i].transform.localPosition.x - value,
+                                        _onPointerExitMoveY), 0.3f).SetEase(Ease.OutBack);
+            trm.DOLocalRotateQuaternion(Quaternion.Euler(0, 0, 0), 0.3f).SetEase(Ease.OutBack);
+        }
     }
 }
